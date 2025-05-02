d32295741ae1849eb5ecca9159848ef228d891b2
Andres Aparicio <aaparici@proteccion.com.co>
2025-05-02 10:58:45 -0500

    Refactor: Update Home component layout and styles, adding content container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,15 @@
-import { useMemo } from 'react'
-import Gallery from '../components/Gallery'
+
 import Title from '../components/UI/Title'
-import useAxios from '../hooks/useAxios'
-import { GET_RANDOM_IMAGES } from '../services/getRandomImages'
 import './styles/Home.scss'
-import Error from '../components/Error'
 
 const Home = () => {
-  const { data: imageList, error, loading, fetch } = useAxios(GET_RANDOM_IMAGES)
-
-  const renderContent = useMemo(() => {
-    if (loading) return <p>Loading...</p>
-    if (error) return <Error onClick={fetch} />
-    if (!imageList) return <p>No images found</p>
-    return <Gallery images={imageList} onChangeImages={fetch} />
-  }, [loading, error, imageList, fetch])
 
   return (
     <main>
       <article className='header-container'>
-        <Title text='Photo Gallery' variant='primary' />
+        <Title text='Main title' variant='primary' />
       </article>
-      <article className='gallery-container'>{renderContent}</article>
+      <article className='content-container'></article>
       <article className='footer-container'>
         <Title text='Made by Andrés Aparicio' variant='secondary' />
         <i
